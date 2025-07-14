cff2b97c3add0fa2ca504098dfa4b496653ea21c
shaftakiha <bangnamoy041@gmail.com>
2025-07-14 15:52:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
 });
 
 client.once('ready', () => {
   console.log(`✅ Bot aktif sebagai ${client.user.tag}`);
 });
 
-client.on('messageCreate', message => {
+client.on('messageCreate', (message) => {
   if (message.content === '!halo') {
     message.channel.send(`Halo ${message.author.username}, aku LorenzoBot!`);
   }
 });
 
-client.login(process.env.TOKEN); // 
+client.login(process.env.TOKEN);
